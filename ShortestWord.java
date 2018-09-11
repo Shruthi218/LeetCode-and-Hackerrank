4ac63b1280f5eef614360414d2a643c186688882
Shruthi218 <sr14m@my.fsu.edu>
2018-09-11 00:38:46 -0400

    Create ShortestWord.java

---
+++ ShortestWord.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+public int shortestDistance(String[] words, String word1, String word2) {
+    int i1 = -1, i2 = -1;
+    int minDistance = Integer.MAX_VALUE;
+    int currentDistance;
+    for (int i = 0; i < words.length; i++) {
+        if (words[i].equals(word1)) {
+            i1 = i;
+        } else if (words[i].equals(word2)) {
+            i2 = i;
+        }
+
+        if (i1 != -1 && i2 != -1) {
+            if(Math.abs(i1-i2) < minDistance) {
+                minDistance = Math.abs(i1-i2);
+            }
+        }
+    }
+    return minDistance;
+}
+}
