abe11655325b168a766192f96280a69c1725158c
Shruthi218 <sr14m@my.fsu.edu>
2018-09-23 23:16:23 -0400

    Recursive approach to convert sorted array to BST

---
+++ SortedArrayToBST.java
@@ -0,0 +1,15 @@
+public class Solution {
+  public TreeNode sortedArrayToBST(int[] nums) {
+    return helper(0,nums.length-1,nums);
+  }
+
+  private TreeNode helper(int left, int right, int[] nums){
+     if(left > right) return null; 
+    int mid = (left+right)/2;
+    TreeNode root = new TreeNode(nums[mid]);
+    root.left = helper(left, mid-1, nums);
+    root.right = helper(mid+1,right, nums);
+    return root;
+    
+  }
+}
