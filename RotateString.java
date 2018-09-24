436a7118ddaba262ba4b5ba8b4fdd2ffd2bc3449
Shruthi218 <sr14m@my.fsu.edu>
2018-09-23 22:44:20 -0400

    Delete RotateString.java

--- RotateString.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public boolean RotateString(String A, String B) {
-        if(A.length() == B.length() && (A + A).contains(B)) {
-            return true;
-    }
-    return false;
-}
-}
