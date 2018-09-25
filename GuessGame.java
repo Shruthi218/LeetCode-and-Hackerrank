c70312eebfa8743942dfc637ab496545d2d57a33
Shruthi218 <sr14m@my.fsu.edu>
2018-09-25 00:42:37 -0400

    Problem #374

---
+++ GuessGame.java
@@ -0,0 +1,20 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            int ans = guess(mid);
+            if(ans==0) return mid;
+            
+            else if(ans == -1) {
+                high = mid-1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
